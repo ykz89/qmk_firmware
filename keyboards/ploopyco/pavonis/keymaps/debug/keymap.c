e0a4ba298d45199c75a5a36818aa437c84ec50d1
George Norton <george_norton_uk@hotmail.com>
2024-07-29 23:31:26 +0100

    Added maxtouch debug interface

--- keyboards/ploopyco/pavonis/keymaps/debug/keymap.c
+++ keyboards/ploopyco/pavonis/keymaps/debug/keymap.c
@@ -3,3 +3,9 @@
 
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {{{ KC_NO }}};
+
+void keyboard_post_init_user(void) {
+    // Customise these values to desired behaviour
+    debug_enable=true;
+    debug_mouse=true;
+}
