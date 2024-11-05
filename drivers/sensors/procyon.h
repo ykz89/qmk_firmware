8c300b7de4c36f19ce9fd24b2f537ca1d87298e8
George Norton <george_norton_uk@hotmail.com>
2024-11-05 16:48:17 +0000

    Fix tuning for 2 finger events.

--- drivers/sensors/procyon.h
+++ drivers/sensors/procyon.h
@@ -15,17 +15,18 @@
 #    ifndef MXT_TOUCH_HYST
 #        define MXT_TOUCH_HYST 10
 #    endif
-#    ifndef MXT_INTERNAL_TOUCH_HYST
-#        define MXT_INTERNAL_TOUCH_HYST 10
-#    endif
 #    ifndef MXT_INTERNAL_TOUCH_THRESHOLD
-#        define MXT_INTERNAL_TOUCH_THRESHOLD 5
+#        define MXT_INTERNAL_TOUCH_THRESHOLD 10
+#    endif
+#    ifndef MXT_INTERNAL_TOUCH_HYST
+#        define MXT_INTERNAL_TOUCH_HYST 5
 #    endif
 #    define MXT_T56_SHIELDLESS_ENABLE
 #    define MXT_RETRANSMISSION_COMPENSATION_ENABLE 0
 #    define MXT_X_PITCH 38 // Procyon boards have a standard 3.8mm pitch
 #    define MXT_Y_PITCH 38
 #    define MXT_ACTIVE_ACQUISITION_INTERVAL 255 // Free run while active - this enables us to reach a polling rate of around 300Hz
+#    define MXT_CONFTHR 5 // Increase debounce time
 #endif
 
 // Board specific configuration
